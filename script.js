830dab8290db4dedb9d00221c03a6adee399f2b9
Santiago de Aguirre <santiago.deaguirre@gmail.com>
2022-01-24 17:48:28 -0300

    Added nav bar and added responsiveness

---
+++ script.js
@@ -0,0 +1,20 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    burger.addEventListener('click', () => {
+        //Toggle Nav
+        nav.classList.toggle('nav-active');
+         //Animate Links
+        navLinks.forEach((link, index)=>{
+        
+        });
+        //Burger Animation
+        burger.classList.toggle('toggle');
+    });
+   
+    
+}
+
+navSlide();
